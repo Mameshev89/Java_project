dbdaf111c7eeb2419ba1f11e5fa68c060822d4c2
Ilnur Mameshev <imameshev@inbox.ru>
2021-10-03 22:49:39 +0300

    add changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int current_balance=2_000_000_000;
-        int sum_transfer=500_000_000;
-        int total=current_balance+sum_transfer;
+        int currentBalance=2_000_000_000;
+        int sumTransfer=500_000_000;
+        int total=currentBalance+sumTransfer;
         System.out.println(total);
     }
 }
